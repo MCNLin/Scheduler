94c8f70bc46580369a6badb87897ef33408abac3
mcnlin <mcnghiem@gmail.com>
2021-12-08 21:56:08 -0500

    add status story

--- stories/index.js
+++ stories/index.js
@@ -15,6 +15,7 @@ import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
 
 
 storiesOf("Button", module)
@@ -160,5 +161,5 @@ onDelete={action("onDelete")} />))
   message= "Delete the appointment?"
   onConfirm={action("onConfirm")}
   onCancel={action("onCancel")}
-  />
- ))
+  />))
+.add("Status", () => <Status message="Deleting"/> )
