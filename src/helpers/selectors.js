a2dc245d09c459cc3c05a814215072b62e7ad52e
mcnlin <mcnghiem@gmail.com>
2021-12-10 19:28:19 -0500

    add getInterview helper function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -13,3 +13,17 @@ export function getAppointmentsForDay(state,day) {
     return appointments;
 };
   
+export function getInterview(state, interview) {
+  
+  if(!interview){
+    return null;
+  }
+
+  const interviews = {
+    student: interview.student,
+    interviewer: state.interviewers[interview.interviewer]
+
+  }
+  return interviews
+}
+
