ff1682f9b4ef1ac7a7320abca8ec3fe7cfc6773d
mcnlin <mcnghiem@gmail.com>
2021-12-13 15:56:16 -0500

    add save function

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -19,6 +19,14 @@ export default function Appointment(props) {
     props.interview ? SHOW : EMPTY
   )
 
+  function save(name, interviewer) {
+    const interview = {
+      student: name,
+      interviewer
+    };
+    props.bookInterview(props.id, interview)
+  }
+
   return (
 
     <article className="appointment">
@@ -31,7 +39,10 @@ export default function Appointment(props) {
         interviewer={props.interview.interviewer}
         />
         )}
-        {mode === CREATE && <Form interviewers={props.interviewers} onCancel={back}/>}
+        {mode === CREATE && <Form 
+        interviewers={props.interviewers} 
+        onCancel={back}
+        onSave={save}/>}
     </article>
 
   )
