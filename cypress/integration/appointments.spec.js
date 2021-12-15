c49446000b8c8d6179c8ffbc37d81220615a53ad
mcnlin <mcnghiem@gmail.com>
2021-12-15 14:37:20 -0500

    add steps to type specific name into input field

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -4,6 +4,16 @@ describe("Appointments", () => {
     
     cy.contains("Monday");
 
-  
-  })
+    cy.get("[alt=Add]")
+      .first()
+      .click();
+
+    cy.get('[data-testid="student-name-input"]')
+      .type("Lydia Miller-Jones");
+    
+    
+  });
+
+
+
 });
