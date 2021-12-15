3517fc2f0f0058777827ecc00bee4fe9abbf416a
mcnlin <mcnghiem@gmail.com>
2021-12-15 12:45:32 -0500

    add datatestid for day

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -4,29 +4,29 @@ import classNames from 'classnames'
 import "components/DayListItem.scss"
 
 export default function DayListItem(props) {
-  let dayClass = classNames('day-list__item',{
+  let dayClass = classNames('day-list__item', {
     'day-list__item--selected': props.selected,
     'day-list__item--full': props.spots === 0
   })
 
   const formatSpots = (spots) => {
-    if(spots === 0){
+    if (spots === 0) {
       return (`no spots remaining`)
-    }else if (spots === 1){
+    } else if (spots === 1) {
       return (`1 spot remaining`)
     }
     return (`${spots} spots remaining`)
-  } 
+  }
 
-    
+
   const availability = formatSpots(props.spots)
 
-  
+
   return (
-    <li className={dayClass} onClick={()=> props.setDay(props.name)} selected={props.selected}>
+    <li className={dayClass} onClick={() => props.setDay(props.name)} selected={props.selected} data-testid='day'>
       <h2 className='text--regular'>{props.name}</h2>
       <h3 className='text--light'>{availability}</h3>
-    
+
     </li>
 
   );
